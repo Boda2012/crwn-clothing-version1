075ecfb929e9142642eba4da756774c6a09b7cb0
Boda2012 <100514011+Boda2012@users.noreply.github.com>
2022-10-21 15:59:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# crwn-clothing-version1
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Boda2012/crwn-clothing-version1)
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation Page!</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
